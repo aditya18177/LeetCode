4e15f8a2346888934247c356c9f43209f458dd9d
aditya18177 <161752730+aditya18177@users.noreply.github.com>
2026-01-27 22:17:35 +0530

    Time: 4 ms (66.9%), Space: 23.7 MB (80.53%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,30 +2,13 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end());
-        int n=intervals.size();
         vector<vector<int>> ans;
-        vector<int> mark(n,1);
-        for(int i=0;i<intervals.size()-1;i++){
-            vector<int> temp;
-            if(intervals[i][1]>=intervals[i+1][0]){
-                temp.push_back(intervals[i][0]);
-                if(intervals[i+1][1]>=intervals[i][1]){
-                    temp.push_back(intervals[i+1][1]);
-                }else{
-                    temp.push_back(intervals[i][1]); 
-                }
-                intervals[i+1][0]=temp[0];
-                intervals[i+1][1]=temp[1];
-                mark[i]=-1;
-            }else{
-                continue;
-            }
-        }
+        int n=intervals.size();
         for(int i=0;i<n;i++){
-            if(mark[i]==-1){
-                continue;
-            }else{
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
                 ans.push_back(intervals[i]);
+            }else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
         }
         return ans;
