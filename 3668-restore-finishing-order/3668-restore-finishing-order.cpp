241121015e0aec32218aa9dd66035cfe4ecfed8b
aditya18177 <161752730+aditya18177@users.noreply.github.com>
2026-02-24 11:59:51 +0530

    Time: 12 ms (5.95%), Space: 50.4 MB (37.85%) - LeetHub

--- 3668-restore-finishing-order/3668-restore-finishing-order.cpp
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
         int n=order.size();
-        unordered_set<int> frq;
+        unordered_set<int> frq(friends.begin(),friends.end());
         vector<int> ans;
-        for(int x:friends){
-            frq.insert(x);
-        }
         for(int z:order){
             if(frq.find(z)!=frq.end()){
                 ans.push_back(z);
