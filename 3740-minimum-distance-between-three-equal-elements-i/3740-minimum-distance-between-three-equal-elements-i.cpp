66cf6fcb41898925e6a72281867304ce2e605be2
aditya18177 <161752730+aditya18177@users.noreply.github.com>
2026-04-05 12:59:12 +0530

    Time: 8 ms (17.53%), Space: 32.9 MB (7.58%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        int ans=INT_MAX;
+        unordered_map<int,int> frq;
+        for(int x:nums){
+            frq[x]++;
+        }
         int n=nums.size();
+        vector<vector<int>> indexes(101);
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[j]==nums[i]){
-                    for(int k=j+1;k<n;k++){
-                        if(nums[k]==nums[i]){
-                            ans=min(ans,abs(i-j)+abs(j-k)+abs(i-k));
-                        }
-                    }
-                }
+            if(frq[nums[i]]>=3){
+                indexes[nums[i]].push_back(i);
+            }
+        }
+        int ans=INT_MAX;
+        for(auto x:indexes){
+            if(x.size()<3) continue;
+            for(int j=0;j<=x.size()-3;j++){
+                ans=min(ans,2 * (x[j+2] - x[j]));
             }
         }
         if(ans==INT_MAX) return -1;
