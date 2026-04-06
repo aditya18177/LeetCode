9ec7755dc656fff82a8419fb1ffce3ab09fb0ef3
aditya18177 <161752730+aditya18177@users.noreply.github.com>
2026-04-06 23:25:53 +0530

    Time: 2 ms (87.4%), Space: 138.8 MB (90.88%) - LeetHub

--- 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cpp
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     long long maximumScore(vector<int>& nums) {
-        int n=nums.size();
-        vector<long long> prefixsum(n,0);
-        vector<int> suffixmin(n,0);
-        long long sum=0;
-        for(int i=0;i<n;i++){
-            prefixsum[i]=sum+nums[i];
-            sum=prefixsum[i];
+        long long sum = 0;
+        for(int x : nums){
+            sum += x;
         }
-        suffixmin[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixmin[i]=min(nums[i],suffixmin[i+1]);
+
+        long long ans = LLONG_MIN;
+        int n = nums.size();
+        int mini = nums[n-1];
+
+        for(int i = n-1; i > 0; i--){
+            mini = min(mini, nums[i]);   
+            sum -= nums[i];              
+
+            long long score = sum - mini;  
+            ans = max(ans, score);
         }
-        long long ans=INT_MIN;
-        for(int i=0;i<n-1;i++){
-            long long score=prefixsum[i]-suffixmin[i+1];
-            ans=max(ans,score);
-        }
+
         return ans;
     }
 };
