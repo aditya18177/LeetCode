2401e2bf34573f3bb8f66e6a87c81e357a19cd5c
aditya18177 <161752730+aditya18177@users.noreply.github.com>
2026-04-09 15:35:21 +0530

    Time: 3 ms (36.43%), Space: 10.2 MB (18.34%) - LeetHub

--- 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
@@ -3,12 +3,10 @@ public:
     int residuePrefixes(string s) {
         int ans=0;
         int n=s.length();
-        string t="";
         unordered_set<char> frq;
         for(int i=0;i<n;i++){
-            t+=s[i];
             frq.insert(s[i]);
-            if(frq.size()==(t.length()%3)){
+            if(frq.size()==((i+1)%3)){
                 ans++;
             }
         }
