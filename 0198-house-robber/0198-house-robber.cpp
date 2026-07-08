53706a4c7d8c18b3a618b986933397a07111e404
aditya18177 <161752730+aditya18177@users.noreply.github.com>
2026-07-08 17:32:21 +0530

    Time: 3 ms (5.99%), Space: 10.7 MB (23.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,9 +5,8 @@ public:
         vector<int> dp(n,0);
         dp[0]=nums[0];
         for(int i=1;i<n;i++){
-            int pick=INT_MIN;
-            if(i-2<0) pick=nums[i];
-            else pick=nums[i]+dp[i-2];
+            int pick=nums[i];
+            if(i>1) pick+=dp[i-2];
             int notpick=dp[i-1];
             dp[i]=max(pick,notpick);
         }
